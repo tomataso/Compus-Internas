0efef75b3b44d498724e6300a9c18c3e2453eb94
Juan Carlos (Tomataso) <tomataso@users.noreply.github.com>
2018-07-04 21:14:03 -0600

    Actualizaciones de Clase Miercoles
    Faltan problemas de apellido, canton, y grado.

--- JuanK Caso de Uso Arquitectonic/public/js/controlador.js
+++ JuanK Caso de Uso Arquitectonic/public/js/controlador.js
@@ -1,209 +1,213 @@
 'use strict';
 
 //Programar .
-imprimirListaProfesor();
+// imprimirListaProfesor();
 
 let botonRegistrar = document.querySelector('#btnRegistrar');
 
-botonRegistrar.addEventListener('click' , obtenerDatos);
+botonRegistrar.addEventListener('click', obtenerDatos);
 
-let inputNombreProfesor = document.querySelector('txtNombreProfesor');
+
+let inputNombreProfesor = document.querySelector('#txtNombreProfesor');
 let inputApellidoProfesor = document.querySelector('#txtApellidoProfesor');
 let inputCedulaProfesor = document.querySelector('#txtCedulaProfesor');
 let inputTelefonoProfesor = document.querySelector('#txtTelefonoProfesor');
 let inputGradoAcademicoProfesor = document.querySelector('#txtGradoAcademico');
-let inputCorreoElectronicoProfesor = document.querySelector('txtCorreoProfesor');
+let inputCorreoElectronicoProfesor = document.querySelector('#txtCorreoProfesor');
 
-let inputProvinciaProfesor = document.querySelector('txtProvinciaProfesor');
-let inputCantonProfesor = document.querySelector('txtCantonProfesor');
-let inputDistritoProfesor = document.querySelector('txtDistritoProfesor');
-let inputDireccionExactaProfesor = document.querySelector('txtDireccionExactaProfesor');
-let inputAnnosExperienciaProfesor = document.querySelector('txtAnnosExperienciaProfesor');
-let inputCursosImpartidos = document.querySelector('txtCursosImpartidos');
-
-
+let inputProvinciaProfesor = document.querySelector('#txtProvinciaProfesor');
+let inputCantonProfesor = document.querySelector('#txtCantonProfesor');
+let inputDistritoProfesor = document.querySelector('#txtDistritoProfesor');
+let inputDireccionExactaProfesor = document.querySelector('#txtDireccionExactaProfesor');
+let inputAnnosExperienciaProfesor = document.querySelector('#txtAnnosExperienciaProfesor');
+let inputCursosImpartidos = document.querySelector('#txtCursosImpartidos');
 
 
-function obtenerDatos(){
-    let infoProfesor =[];
+
+
+function obtenerDatos() {
+    let infoProfesor = [];
     let bError = false;
 
-    let sNombreProfesor = inputNombreProfesor.value
-    let sApellidoProfesor =  inputApellidoProfesor.value  
+    let sNombreProfesor = inputNombreProfesor.value;
+    let sApellidoProfesor = inputApellidoProfesor.value;
     let sCedulaProfesor = inputCedulaProfesor.value;
     let sTelefonoProfesor = inputTelefonoProfesor.value;
     let sGradoAcademicoProfesor = inputGradoAcademicoProfesor.value;
     let sCorreoElectronicoProfesor = inputCorreoElectronicoProfesor.value;
 
-    let sProvinciaProfesor = inputProvinciaProfesor.value ;
-    let sCantonProfesor = inputCantonProfesor.value ;
-    let sDistritoProfesor = inputDistritoProfesor.value ;
-    let sDireccionExactaProfesor = inputDireccionExactaProfesor.value ;
-    let sAnnosExperienciaProfesor = inputAnnosExperienciaProfesor.value ;
-    let sCursosImpartidos = inputCursosImpartidos.value ;
+    let sProvinciaProfesor = inputProvinciaProfesor.value;
+    let sCantonProfesor = inputCantonProfesor.value;
+    let sDistritoProfesor = inputDistritoProfesor.value;
+    let sDireccionExactaProfesor = inputDireccionExactaProfesor.value;
+    let sAnnosExperienciaProfesor = inputAnnosExperienciaProfesor.value;
+    let sCursosImpartidos = inputCursosImpartidos.value;
 
-    infoProfesor.push(sNombreProfesor, sApellidoProfesor, sCedulaProfesor, sTelefonoProfesor, sGradoAcademicoProfesor, sCorreoElectronicoProfesor, sProvinciaProfesor, sCantonProfesor, sDistritoProfesor, sDireccionExactaProfesor, sAnnosExperienciaProfesor,sCursosImpartidos );
-    
+    infoProfesor.push(sNombreProfesor, sApellidoProfesor, sCedulaProfesor, sTelefonoProfesor, sGradoAcademicoProfesor, sCorreoElectronicoProfesor, sProvinciaProfesor, sCantonProfesor, sDistritoProfesor, sDireccionExactaProfesor, sAnnosExperienciaProfesor, sCursosImpartidos);
+
     bError = validar();
-    if(bError == true){
+
+    if (bError == true) {
         swal({
-            type : 'warning',
-            title : 'No se pudo registrar el usuario profesor.',
+            type: 'warning',
+            title: 'No se pudo registrar el usuario profesor.',
             text: 'Por favor revise los campos en rojo',
-            confirmButtonText : 'Entendido'
+            confirmButtonText: 'Entendido'
         });
         console.log('No se pudo registrar el usuario profesor');
-    }else{
+    } else {
         registrarCliente(infoProfesor);
         swal({
-            type : 'success',
-            title : 'Registro del profesor exitoso',
+            type: 'success',
+            title: 'Registro del profesor exitoso',
             text: 'El profesor usuario se registró adecuadamente',
-            confirmButtonText : 'Entendido'
+            confirmButtonText: 'Entendido'
         });
         limpiarFormularioProfesor();
     }
-    
+
 };
 
-function validar(){
+function validar() {
     let bError = false;
 
     let regexSoloLetras = /^[a-z A-ZáéíóúÁÉÍÓÚñÑ]+$/;
     let regexSoloNumeros = /^[0-9]{1,3}$/;
 
     //Validación del NombreProfesor
-    if(inputNombreProfesor.value == '' || (regexSoloLetras.test(inputNombreProfesor.value)==false) ){
+    if (inputNombreProfesor.value == '' || (regexSoloLetras.test(inputNombreProfesor.value) == false)) {
         inputNombreProfesor.classList.add('error_input');
         bError = true;
-    }else{
+    } else {
         inputNombreProfesor.classList.remove('error_input');
     }
 
 
     //Validación del ApellidoProfesor
-    if(inputApellidoProfesor.value == ''|| (regexSoloLetras.test(inputApellidoProfesor.value)==false) ){
+    if (inputApellidoProfesor.value == '' || (regexSoloLetras.test(inputApellidoProfesor.value) == false)) {
         inputApellidoProfesor.classList.add('error_input');
         bError = true;
-    }else{
+    } else {
         inputApellidoProfesor.classList.remove('error_input');
     }
 
     //Validación de la CedulaProfesor
-    if(inputCedulaProfesor.value == ''|| (regexSoloNumeros.test(inputCedulaProfesor.value)==false) ){
+    if (inputCedulaProfesor.value == '' ) {
         inputCedulaProfesor.classList.add('error_input');
         bError = true;
-    }else{
+    } else {
         inputCedulaProfesor.classList.remove('error_input');
     }
 
     //Validación de la TelefonoProfesor
-    if(inputTelefonoProfesor.value == '' || (regexSoloNumeros.test(inputTelefonoProfesor.value) == false) ){
+    if (inputTelefonoProfesor.value == '' ) {
         inputTelefonoProfesor.classList.add('error_input');
         bError = true;
-    }else{
+    } else {
         inputTelefonoProfesor.classList.remove('error_input');
     }
 
 
     //Validación del  GradoAcademicoProfesor
-    if(inputGradoAcademicoProfesor.value == '' ){
+    if (inputGradoAcademicoProfesor.value == '') {
         inputGradoAcademicoProfesor.classList.add('error_input');
         bError = true;
-    }else{
+    } else {
         inputGradoAcademicoProfesor.classList.remove('error_input');
     }
 
 
 
     //Validación de la CorreoProfesor
-    if(inputCorreoElectronicoProfesor.value == '' ){
+    if (inputCorreoElectronicoProfesor.value == '') {
         inputCorreoElectronicoProfesor.classList.add('error_input');
         bError = true;
-    }else{
+    } else {
         inputCorreoElectronicoProfesor.classList.remove('error_input');
     }
 
 
     //Validación del ProvinciaProfesor
-    if(inputProvinciaProfesor.value == '' || (regexSoloLetras.test(inputProvinciaProfesor.value)==false) ){
+    if (inputProvinciaProfesor.value == '' || (regexSoloLetras.test(inputProvinciaProfesor.value) == false)) {
         inputProvinciaProfesor.classList.add('error_input');
         bError = true;
-    }else{
+    } else {
         inputNombreProfesor.classList.remove('error_input');
     }
 
 
     //Validación del  CantonProfesor
-    if(inputCantonProfesor.value == '' ){
-          inputCantonProfesor.classList.add('error_input');
-           bError = true;
-    }else {            inputCantonProfesor.classList.remove('error_input');
+    if (inputCantonProfesor.value == '') {
+        inputCantonProfesor.classList.add('error_input');
+        bError = true;
+    } else {
+        inputCantonProfesor.classList.remove('error_input');
     }
-    
+
 
     //Validación del  CantonProfesor
-    if(inputCantonProfesor.value == '' ){
-          inputCantonProfesor.classList.add('error_input');
-           bError = true;
-    }else {            inputCantonProfesor.classList.remove('error_input');
+    if (inputCantonProfesor.value == '') {
+        inputCantonProfesor.classList.add('error_input');
+        bError = true;
+    } else {
+        inputCantonProfesor.classList.remove('error_input');
     }
 
 
-     //Validación del  DistritoProfesor
-    if(inputDistritoProfesor.value == '' ){
+    //Validación del  DistritoProfesor
+    if (inputDistritoProfesor.value == '') {
         inputDistritoProfesor.classList.add('error_input');
-            bError = true;
-     }else{
-         inputDistritoProfesor.classList.remove('error_input');
-     }
+        bError = true;
+    } else {
+        inputDistritoProfesor.classList.remove('error_input');
+    }
 
-     //Validación del  DireccionExactaProfesor
-     if(inputDireccionExactaProfesor.value == '' ){
+    //Validación del  DireccionExactaProfesor
+    if (inputDireccionExactaProfesor.value == '') {
         inputDireccionExactaProfesor.classList.add('error_input');
-            bError = true;
-     }else{
+        bError = true;
+    } else {
         inputDireccionExactaProfesor.classList.remove('error_input');
-     }
+    }
 
-   //Validación de la AnnosExperienciaProfesor
-   if(inputAnnosExperienciaProfesor.value == '' || (regexSoloNumeros.test(inputAnnosExperienciaProfesor.value) == false) ){
-    inputAnnosExperienciaProfesor.classList.add('error_input');
-            bError = true;
-    }else{
+    //Validación de la AnnosExperienciaProfesor
+    if (inputAnnosExperienciaProfesor.value == '' || (regexSoloNumeros.test(inputAnnosExperienciaProfesor.value) == false)) {
+        inputAnnosExperienciaProfesor.classList.add('error_input');
+        bError = true;
+    } else {
         inputAnnosExperienciaProfesor.classList.remove('error_input');
     }
 
-     //Validación del  CursosImpartidos
-     if(inputCursosImpartidos.value == '' ){
+    //Validación del  CursosImpartidos
+    if (inputCursosImpartidos.value == '') {
         inputCursosImpartidos.classList.add('error_input');
-            bError = true;
-     }else{
+        bError = true;
+    } else {
         inputCursosImpartidos.classList.remove('error_input');
-     }
+    }
 
 
 
     return bError;
 };
 
-function limpiarFormularioProfesor(){
- 
+function limpiarFormularioProfesor() {
+
 
     inputNombreProfesor.value = '';
     inputApellidoProfesor.value = '';
     inputCedulaProfesor.value = '';
-    inputTelefonoProfesor.value = ''; 
+    inputTelefonoProfesor.value = '';
     inputGradoAcademicoProfesor.value = '';
     inputCorreoElectronicoProfesor.value = '';
-    
+
     inputProvinciaProfesor.value = '';
-    inputCantonProfesor.value = '' ;
-    inputDistritoProfesor.value = '' ;
+    inputCantonProfesor.value = '';
+    inputDistritoProfesor.value = '';
     inputDireccionExactaProfesor.value = '';
     inputAnnosExperienciaProfesor.value = '';
     inputCursosImpartidos.value = '';
 
-   
+
 
 }
